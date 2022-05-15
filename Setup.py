6a6823df7796da9ebca25315760481f71829721c
Christopher Mena <mena.christopher.student@gmail.com>
2022-05-15 19:16:43 -0400

    Working Setup.py

--- Setup.py
+++ Setup.py
@@ -2,10 +2,11 @@ import subprocess
 import os
 import os.path
 import sys
+import tarfile
 
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip3", "install", package])
+def install(packages):
+    for package in packages:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 def checkIfFile():
     file_path = os.getcwd()
@@ -15,5 +16,16 @@ def checkIfFile():
     else:
         print('File exists')
 
+def download():
+    try:
+        import wget
+    except:
+        print('Error')
+    url = 'http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
+    file_get = wget.download(url)
+    file_tar = tarfile.open('aclImdb_v1.tar.gz')
+    file_tar.extractall()
+
 if __name__ == "__main__":
-    checkIfFile()
+    packages =  ['wget', 'numpy', 'panda', 'seaborn', 'matplotlib', 'jupyterlab', 'selenium', 'tensorflow', 'scikit-learn', 'kivy']
+    install(packages)
