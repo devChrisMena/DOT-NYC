f91d5680f89128d0ba3190fa9e5ca4532f4cf112
Christopher Mena <mena.christopher.student@gmail.com>
2022-05-15 17:31:54 -0400

    Setup.py created

---
+++ Setup.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+import os.path
+import sys
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip3", "install", package])
+
+def checkIfFile():
+    file_path = os.getcwd()
+    # check if file exist:
+    if not os.path.exists('aclImdb_v1.tar.gz'):
+        print('File does not exist ')
+    print(directory)
+
+if __name__ == "__main__":
+    checkIfFile()
