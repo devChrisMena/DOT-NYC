cd0aa83fc18786064eb527707097e2a7594423e4
Christopher Mena <mena.christopher.student@gmail.com>
2022-05-16 10:48:36 -0400

    added comments

--- Download.py
+++ Download.py
@@ -5,18 +5,30 @@ import tarfile
 import wget
 
 def download():
+    '''
+    Downloads sentiment library
+    '''
     url = 'http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
     file_get = wget.download(url)
 
 def extract():
+    '''
+    Extracts sentiment library
+    '''
     file_tar = tarfile.open('aclImdb_v1.tar.gz')
     file_tar.extractall()
 
 def remove():
+    '''
+    Removes sentiment file & Directory
+    '''
     os.remove('aclImdb_v1.tar')
     shutil.rmtree('aclImdb')
 
 def checkIfFile():
+    '''
+    Check if sentiment library and Directory exist
+    '''
     file_path = os.getcwd()
     # check if file exist:
     if not os.path.exists('aclImdb_v1.tar.gz'):
